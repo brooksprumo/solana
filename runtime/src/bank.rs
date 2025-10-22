cb5c69665a50260d9a5ae458d6cbdeb8f27a8e4a
brooks <brooks@anza.xyz>
2025-10-22 10:01:02 -0400

    Reduces MAX_LEADER_SCHEDULE_STAKES to 3

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -212,7 +212,7 @@ pub(crate) mod tests;
 
 pub const SECONDS_PER_YEAR: f64 = 365.25 * 24.0 * 60.0 * 60.0;
 
-pub const MAX_LEADER_SCHEDULE_STAKES: Epoch = 5;
+pub const MAX_LEADER_SCHEDULE_STAKES: Epoch = 3;
 
 pub type BankStatusCache = StatusCache<Result<()>>;
 #[cfg_attr(
