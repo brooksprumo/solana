dcd123218289200f0ff8a3876715c68bfb00bc6a
brooks <brooks@anza.xyz>
2025-11-17 10:07:29 -0500

    adds cmp_snapshot_archive_kinds_by_priority

--- runtime/src/snapshot_package/compare.rs
+++ runtime/src/snapshot_package/compare.rs
@@ -3,35 +3,40 @@ use {
     std::cmp::Ordering::{self, Equal, Greater, Less},
 };
 
-/// Compare snapshot packages by priority; first by type, then by slot
+/// Compare snapshot packages by priority; first by kind, then by slot
 #[must_use]
 pub fn cmp_snapshot_packages_by_priority(a: &SnapshotPackage, b: &SnapshotPackage) -> Ordering {
     cmp_snapshot_kinds_by_priority(&a.snapshot_kind, &b.snapshot_kind).then(a.slot.cmp(&b.slot))
 }
 
 /// Compare snapshot kinds by priority
-///
-/// Full snapshots are higher in priority than incremental snapshots.
-/// If two `IncrementalSnapshot`s are compared, their base slots are the tiebreaker.
 #[must_use]
 pub fn cmp_snapshot_kinds_by_priority(a: &SnapshotKind, b: &SnapshotKind) -> Ordering {
     use SnapshotKind as Kind;
     match (a, b) {
-        (Kind::Archive(SnapshotArchiveKind::Full), Kind::Archive(SnapshotArchiveKind::Full)) => {
-            Equal
+        (Kind::Archive(snapshot_archive_kind_a), Kind::Archive(snapshot_archive_kind_b)) => {
+            cmp_snapshot_archive_kinds_by_priority(snapshot_archive_kind_a, snapshot_archive_kind_b)
         }
-        (
-            Kind::Archive(SnapshotArchiveKind::Full),
-            Kind::Archive(SnapshotArchiveKind::Incremental(_)),
-        ) => Greater,
-        (
-            Kind::Archive(SnapshotArchiveKind::Incremental(_)),
-            Kind::Archive(SnapshotArchiveKind::Full),
-        ) => Less,
-        (
-            Kind::Archive(SnapshotArchiveKind::Incremental(base_slot_a)),
-            Kind::Archive(SnapshotArchiveKind::Incremental(base_slot_b)),
-        ) => base_slot_a.cmp(base_slot_b),
+    }
+}
+
+/// Compare snapshot archive kinds by priority
+///
+/// Full snapshot archives are higher in priority than incremental snapshot archives.
+/// If two `Incremental`s are compared, their base slots are the tiebreaker.
+#[must_use]
+pub fn cmp_snapshot_archive_kinds_by_priority(
+    a: &SnapshotArchiveKind,
+    b: &SnapshotArchiveKind,
+) -> Ordering {
+    use SnapshotArchiveKind as Kind;
+    match (a, b) {
+        (Kind::Full, Kind::Full) => Equal,
+        (Kind::Full, Kind::Incremental(_)) => Greater,
+        (Kind::Incremental(_), Kind::Full) => Less,
+        (Kind::Incremental(base_slot_a), Kind::Incremental(base_slot_b)) => {
+            base_slot_a.cmp(base_slot_b)
+        }
     }
 }
 
@@ -182,6 +187,44 @@ mod tests {
             ),
         ] {
             let actual_result = cmp_snapshot_kinds_by_priority(&snapshot_kind_a, &snapshot_kind_b);
+            assert_eq!(expected_result, actual_result);
+        }
+    }
+
+    #[test]
+    fn test_cmp_snapshot_archive_kinds_by_priority() {
+        for (snapshot_archive_kind_a, snapshot_archive_kind_b, expected_result) in [
+            (SnapshotArchiveKind::Full, SnapshotArchiveKind::Full, Equal),
+            (
+                SnapshotArchiveKind::Full,
+                SnapshotArchiveKind::Incremental(5),
+                Greater,
+            ),
+            (
+                SnapshotArchiveKind::Incremental(5),
+                SnapshotArchiveKind::Full,
+                Less,
+            ),
+            (
+                SnapshotArchiveKind::Incremental(5),
+                SnapshotArchiveKind::Incremental(6),
+                Less,
+            ),
+            (
+                SnapshotArchiveKind::Incremental(5),
+                SnapshotArchiveKind::Incremental(5),
+                Equal,
+            ),
+            (
+                SnapshotArchiveKind::Incremental(5),
+                SnapshotArchiveKind::Incremental(4),
+                Greater,
+            ),
+        ] {
+            let actual_result = cmp_snapshot_archive_kinds_by_priority(
+                &snapshot_archive_kind_a,
+                &snapshot_archive_kind_b,
+            );
             assert_eq!(expected_result, actual_result);
         }
     }
