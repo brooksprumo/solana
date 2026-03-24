0aa4888df4cd808f7f018afa577b87575924fdaf
brooks <brooks@anza.xyz>
2026-03-24 13:55:30 -0400

    wip: use siphash1-3 for pubkey binning

--- accounts-db/src/pubkey_bins.rs
+++ accounts-db/src/pubkey_bins.rs
@@ -1,48 +1,87 @@
 use {
     solana_pubkey::Pubkey,
-    std::num::{NonZeroU32, NonZeroUsize},
+    std::hash::{self, BuildHasher as _},
+    std::{fmt, num::NonZeroUsize},
 };
 
-#[derive(Clone, Copy, Debug)]
-pub struct PubkeyBinCalculator24 {
-    // how many bits from the first 3 bytes to shift away to ignore when calculating bin
-    shift_bits: u32,
+/// Used to calculate which bin a pubkey maps to.
+///
+/// This struct may be cloned, and will retain the same pubkey -> bin results.
+///
+/// To instantiate, use `PubkeyBinCalculatorBuilder::with_bins(num_bins)`.
+#[derive(Clone)]
+pub struct PubkeyBinCalculator {
+    mask: u64,
+    hasher: hash::RandomState,
 }
 
-impl PubkeyBinCalculator24 {
-    const MAX_BITS: u32 = 24;
-    const MAX_BINS: usize = 1_usize << Self::MAX_BITS;
+impl PubkeyBinCalculator {
+    /// Calculates the bin that `pubkey` maps to.
+    #[inline]
+    pub fn bin_from_pubkey(&self, pubkey: &Pubkey) -> usize {
+        let hash = self.hash_from_pubkey(pubkey);
+        (hash & self.mask) as usize
+    }
 
-    /// Creates a new PubkeyBinCalculator24
+    /// Calculates the hash of `pubkey`.
+    #[inline]
+    fn hash_from_pubkey(&self, pubkey: &Pubkey) -> u64 {
+        self.hasher.hash_one(pubkey)
+    }
+}
+
+impl fmt::Debug for PubkeyBinCalculator {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("PubkeyBinCalculator")
+            .field("num_bins", &(self.mask + 1))
+            .finish_non_exhaustive()
+    }
+}
+
+/// Used to build unique instances of `PubkeyBinCalculator'.
+#[derive(Debug)]
+pub struct PubkeyBinCalculatorBuilder;
+
+impl PubkeyBinCalculatorBuilder {
+    /// Builds a `PubkeyBinCalculator` with `num_bins`.
+    ///
+    /// The returned bin calculator will produce *unique* mappings
+    /// compared to other bin calculators!
     ///
     /// # Panics
     ///
     /// This function will panic if the following conditions are not met:
-    /// * `bins` must be greater than zero
-    /// * `bins` must be a power of two
-    /// * `bins` must be less than or equal to 2^24
-    pub fn new(bins: usize) -> Self {
-        // SAFETY: Caller must guarantee `bins` is non-zero.
-        let bins = NonZeroUsize::new(bins).expect("bins is non-zero");
-        assert!(bins.is_power_of_two());
-        assert!(bins.get() <= Self::MAX_BINS);
-        // SAFETY: `bins` was already non-zero, and we just asserted it fits in 24 bits.
-        let bins = unsafe { NonZeroU32::new_unchecked(bins.get() as u32) };
-        let bits = bins.ilog2();
-        Self {
-            shift_bits: Self::MAX_BITS - bits,
+    /// * `num_bins` must be a power of two
+    pub fn with_bins(num_bins: NonZeroUsize) -> PubkeyBinCalculator {
+        Self::build(num_bins, hash::RandomState::new())
+    }
+
+    /// Builds a `PubkeyBinCalculator` with `num_bins` and `offset`.
+    ///
+    /// The `offset` is used to instantiate a specific PubkeyHasher for the bin calculator.
+    /// Prefer `with_bins()` whenever possible.
+    ///
+    /// The returned bin calculator will produce *identical* mappings
+    /// compared to other bin calculators with the same num_bins and offset.
+    ///
+    /// # Panics
+    ///
+    /// This function will panic if the following conditions are not met:
+    /// * `num_bins` must be a power of two
+    pub fn with_bins_and_offset(num_bins: NonZeroUsize, offset: usize) -> PubkeyBinCalculator {
+        unimplemented!()
+    }
+
+    /// Internal helper for building a `PubkeyBinCalculator`.
+    ///
+    /// Only intended to be called by the public build methods.
+    fn build(num_bins: NonZeroUsize, hasher: hash::RandomState) -> PubkeyBinCalculator {
+        assert!(num_bins.is_power_of_two());
+        let num_bins_mask = num_bins.get() - 1;
+        PubkeyBinCalculator {
+            mask: num_bins_mask as u64,
+            hasher,
         }
-    }
-
-    pub fn bins(&self) -> usize {
-        1 << (Self::MAX_BITS - self.shift_bits)
-    }
-
-    #[inline]
-    pub fn bin_from_pubkey(&self, pubkey: &Pubkey) -> usize {
-        let as_ref = pubkey.as_ref();
-        (((as_ref[0] as usize) << 16) | ((as_ref[1] as usize) << 8) | (as_ref[2] as usize))
-            >> self.shift_bits
     }
 }
 
@@ -50,174 +89,63 @@ impl PubkeyBinCalculator24 {
 mod tests {
     use super::*;
 
-    impl PubkeyBinCalculator24 {
-        fn lowest_pubkey_from_bin(&self, mut bin: usize) -> Pubkey {
-            assert!(bin < self.bins());
-            bin <<= self.shift_bits;
-            let mut pubkey = Pubkey::from([0; 32]);
-            pubkey.as_mut()[0] = ((bin / 256 / 256) & 0xff) as u8;
-            pubkey.as_mut()[1] = ((bin / 256) & 0xff) as u8;
-            pubkey.as_mut()[2] = (bin & 0xff) as u8;
-            pubkey
-        }
-
-        fn highest_pubkey_from_bin(&self, mut bin: usize) -> Pubkey {
-            assert!(bin < self.bins());
-            let mask = (1 << self.shift_bits) - 1;
-            bin <<= self.shift_bits;
-            bin |= mask;
-            let mut pubkey = Pubkey::from([0xff; 32]);
-            pubkey.as_mut()[0] = ((bin / 256 / 256) & 0xff) as u8;
-            pubkey.as_mut()[1] = ((bin / 256) & 0xff) as u8;
-            pubkey.as_mut()[2] = (bin & 0xff) as u8;
-            pubkey
-        }
-    }
-
+    /// Ensure that bin calculation is deterministic.
     #[test]
-    fn test_pubkey_bins() {
-        for i in 0..=24 {
-            let bins = 2u32.pow(i);
-            let calc = PubkeyBinCalculator24::new(bins as usize);
-            assert_eq!(calc.shift_bits, 24 - i, "i: {i}");
-            for bin in 0..bins {
-                assert_eq!(
-                    bin as usize,
-                    calc.bin_from_pubkey(&calc.lowest_pubkey_from_bin(bin as usize))
-                );
-
-                assert_eq!(
-                    bin as usize,
-                    calc.bin_from_pubkey(&calc.highest_pubkey_from_bin(bin as usize))
-                );
-
-                assert_eq!(calc.bins(), bins as usize);
-            }
-        }
-    }
-
-    #[test]
-    fn test_pubkey_bins_pubkeys() {
-        let mut pk = Pubkey::from([0; 32]);
-        for i in 0..=8 {
-            let bins = 2usize.pow(i);
-            let calc = PubkeyBinCalculator24::new(bins);
-
-            let shift_bits = calc.shift_bits - 16; // we are only dealing with first byte
-
-            pk.as_mut()[0] = 0;
-            assert_eq!(0, calc.bin_from_pubkey(&pk));
-            pk.as_mut()[0] = 0xff;
-            assert_eq!(bins - 1, calc.bin_from_pubkey(&pk));
-
-            for bin in 0..bins {
-                pk.as_mut()[0] = (bin << shift_bits) as u8;
-                assert_eq!(
-                    bin,
-                    calc.bin_from_pubkey(&pk),
-                    "bin: {}/{}, shift_bits: {}, val: {}",
-                    bin,
-                    bins,
-                    shift_bits,
-                    pk.as_ref()[0]
-                );
-                if bin > 0 {
-                    pk.as_mut()[0] = ((bin << shift_bits) - 1) as u8;
-                    assert_eq!(bin - 1, calc.bin_from_pubkey(&pk));
+    fn test_bin_from_pubkey_is_deterministic() {
+        for num_bins in [1 << 10, 1 << 14, 1 << 19] {
+            let bin_calculator1 =
+                PubkeyBinCalculatorBuilder::with_bins(NonZeroUsize::new(num_bins).unwrap());
+            // second bin calculator that exercies Calculator::clone()
+            let bin_calculator2 = bin_calculator1.clone();
+            for i_pubkey in 0..1_000 {
+                let pubkey = Pubkey::new_unique();
+                let expected_bin = bin_calculator1.bin_from_pubkey(&pubkey);
+                for i_calculation in 0..10 {
+                    let actual_bin = bin_calculator1.bin_from_pubkey(&pubkey);
+                    assert_eq!(
+                        actual_bin, expected_bin,
+                        "num_bins: {num_bins}, i_pubkey: {i_pubkey}, i_calculation: \
+                         {i_calculation}, pubkey: {pubkey}",
+                    );
                 }
-            }
-        }
-
-        for i in 9..=16 {
-            let mut pk = Pubkey::from([0; 32]);
-            let bins = 2usize.pow(i);
-            let calc = PubkeyBinCalculator24::new(bins);
-
-            let shift_bits = calc.shift_bits - 8;
-
-            pk.as_mut()[1] = 0;
-            assert_eq!(0, calc.bin_from_pubkey(&pk));
-            pk.as_mut()[0] = 0xff;
-            pk.as_mut()[1] = 0xff;
-            assert_eq!(bins - 1, calc.bin_from_pubkey(&pk));
-
-            let mut pk = Pubkey::from([0; 32]);
-            for bin in 0..bins {
-                let mut target = (bin << shift_bits) as u16;
-                pk.as_mut()[0] = (target / 256) as u8;
-                pk.as_mut()[1] = (target % 256) as u8;
-                assert_eq!(
-                    bin,
-                    calc.bin_from_pubkey(&pk),
-                    "bin: {}/{}, shift_bits: {}, val: {}",
-                    bin,
-                    bins,
-                    shift_bits,
-                    pk.as_ref()[0]
-                );
-                if bin > 0 {
-                    target -= 1;
-                    pk.as_mut()[0] = (target / 256) as u8;
-                    pk.as_mut()[1] = (target % 256) as u8;
-                    assert_eq!(bin - 1, calc.bin_from_pubkey(&pk));
-                }
-            }
-        }
-
-        for i in 17..=24 {
-            let mut pk = Pubkey::from([0; 32]);
-            let bins = 2usize.pow(i);
-            let calc = PubkeyBinCalculator24::new(bins);
-
-            let shift_bits = calc.shift_bits;
-
-            pk.as_mut()[1] = 0;
-            assert_eq!(0, calc.bin_from_pubkey(&pk));
-            pk.as_mut()[0] = 0xff;
-            pk.as_mut()[1] = 0xff;
-            pk.as_mut()[2] = 0xff;
-            assert_eq!(bins - 1, calc.bin_from_pubkey(&pk));
-
-            let mut pk = Pubkey::from([0; 32]);
-            for bin in 0..bins {
-                let mut target = (bin << shift_bits) as u32;
-                pk.as_mut()[0] = (target / 256 / 256) as u8;
-                pk.as_mut()[1] = ((target / 256) % 256) as u8;
-                pk.as_mut()[2] = (target % 256) as u8;
-                assert_eq!(
-                    bin,
-                    calc.bin_from_pubkey(&pk),
-                    "bin: {}/{}, shift_bits: {}, val: {:?}",
-                    bin,
-                    bins,
-                    shift_bits,
-                    &pk.as_ref()[0..3],
-                );
-                if bin > 0 {
-                    target -= 1;
-                    pk.as_mut()[0] = (target / 256 / 256) as u8;
-                    pk.as_mut()[1] = ((target / 256) % 256) as u8;
-                    pk.as_mut()[2] = (target % 256) as u8;
-                    assert_eq!(bin - 1, calc.bin_from_pubkey(&pk));
-                }
+                assert_eq!(expected_bin, bin_calculator2.bin_from_pubkey(&pubkey));
             }
         }
     }
 
+    /// Ensure that bin calculators from *different* builders produce different hashes.
     #[test]
-    #[should_panic(expected = "bins.is_power_of_two()")]
-    fn test_pubkey_bins_bad_non_pow2() {
-        PubkeyBinCalculator24::new(3);
+    fn test_builders_produces_unique_instances() {
+        let num_bins = NonZeroUsize::new(1).unwrap();
+        let bin_calculator1 = PubkeyBinCalculatorBuilder::with_bins(num_bins);
+        let bin_calculator2 = PubkeyBinCalculatorBuilder::with_bins(num_bins);
+        let pubkey = Pubkey::new_unique();
+        assert_ne!(
+            bin_calculator1.hash_from_pubkey(&pubkey),
+            bin_calculator2.hash_from_pubkey(&pubkey),
+        );
     }
 
+    /// Ensure that bin calculators from different builders, but with the
+    /// same num_bins and offset, produce *identical* hashes.
     #[test]
-    #[should_panic(expected = "bins.get() <= Self::MAX_BINS")]
-    fn test_pubkey_bins_bad_too_large() {
-        PubkeyBinCalculator24::new(1 << (PubkeyBinCalculator24::MAX_BITS + 1));
+    fn test_builders_with_same_offset_produce_identical_instances() {
+        let num_bins = NonZeroUsize::new(1).unwrap();
+        let offset = 0;
+        let bin_calculator1 = PubkeyBinCalculatorBuilder::with_bins_and_offset(num_bins, offset);
+        let bin_calculator2 = PubkeyBinCalculatorBuilder::with_bins_and_offset(num_bins, offset);
+        let pubkey = Pubkey::new_unique();
+        assert_eq!(
+            bin_calculator1.hash_from_pubkey(&pubkey),
+            bin_calculator2.hash_from_pubkey(&pubkey),
+        );
     }
+
+    /// Ensure non-power-of-two number of bins is not allowed.
     #[test]
-    #[should_panic(expected = "bins is non-zero")]
-    fn test_pubkey_bins_bad_is_zero() {
-        PubkeyBinCalculator24::new(0);
+    #[should_panic(expected = "num_bins.is_power_of_two()")]
+    fn test_num_bins_not_power_of_two_should_panic() {
+        let num_bins = NonZeroUsize::new(3).unwrap();
+        PubkeyBinCalculatorBuilder::with_bins(num_bins);
     }
 }
